2e2f77905675ea286fd436837d8f74bec82c8965
atgdev <atgdev@localhost.localdomain>
2017-11-03 11:42:38 -0700

    Set -m as default for fuzzy find and added binding for select/deselect all elements Added script file to handle functions

--- .bashrc.d/09_fzf.sh
+++ .bashrc.d/09_fzf.sh
@@ -10,3 +10,7 @@ export FZF_DEFAULT_OPTS='
   --color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108
   --color info:108,prompt:109,spinner:108,pointer:168,marker:168
   '
+
+# binding select-all | deselect-all | toggle-all
+# and adding multiple search by default
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS-m --bind ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all"
